5f7e117dbe242ae0c86459f5bf812041d13940e9
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-13 18:28:57 +0300

    creating queue class

--- Implement Stack using Queues/stack_using_queue.py
+++ Implement Stack using Queues/stack_using_queue.py
@@ -1,1 +1,26 @@
-
+class Queue:
+    def __init__(self, data= None) -> None:
+        self.data = data
+        self.next = None
+    def push(self, x):
+        if self.data is None:
+            self.data = x
+        else:
+            curr = self
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = Queue(x)
+    def peek(self):
+        return self.data
+    def pop(self):
+        popped = Queue(self.data)
+        if self.next is None:
+            self.data = None
+            return popped.data
+        self.data = self.next.data
+        self.next = self.next.next
+        return popped.data
+    def empty(self):
+        if self.data is None:
+            return True
+        return False
