c15b8f4db896ba3675e715762696ec828d5a8add
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-13 18:56:07 +0300

    making basic stack

--- Maximum Frequency Stack/max_frequen_stack.py
+++ Maximum Frequency Stack/max_frequen_stack.py
@@ -1,1 +1,23 @@
-
+class FreqStack:
+    def __init__(self, data= None):
+        self.data = data
+        self.next = None 
+    def push(self, val: int) -> None:
+        if self.data is None:
+            self.data = val
+        else:
+            curr = self
+            while curr.next != None:
+                curr = curr.next
+            curr.next = FreqStack(val)
+    def pop(self) -> int:
+        if self.next is None:
+            popped = FreqStack(self.data)
+            self.data = None
+            return popped.data
+        curr = self
+        while curr.next.next is not None:
+            curr = curr.next
+        popped = FreqStack(curr.next.data)
+        curr.next = None
+        return popped.data
