752d4bade32d89aff8bb94d3ceae5bbc43acc10a
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-13 16:51:41 +0300

    creating stack class using linked list

--- mplement Queue using Stacks/queuse_using_stack.py
+++ mplement Queue using Stacks/queuse_using_stack.py
@@ -1,1 +1,30 @@
-
+class Stack:
+    def __init__(self, data= None) -> None:
+        self.data = data
+        self.next = None
+    def push(self, x):
+        if self.data is None:
+            self.data = x
+        else:
+            curr = self
+            while curr.next != None:
+                curr = curr.next
+            curr.next = Stack(x)
+    def pop(self):
+        popped = self.data
+        if self.next is None:
+            self.data = None
+            return popped
+        curr = self
+        while curr.next.next is not None:
+            curr = curr.next
+        curr.next = None
+    def peek(self):
+        curr = self
+        while curr.next is not None:
+            curr = curr.next
+        return curr.data
+    def empty(self):
+        if self.data is None:
+            return True
+        return False
