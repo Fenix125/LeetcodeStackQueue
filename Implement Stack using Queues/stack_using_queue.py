19466b56d097ecfbc5c8d84623e0ae963ef71ac4
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-13 18:35:10 +0300

    some ideas of implementaions

--- Implement Stack using Queues/stack_using_queue.py
+++ Implement Stack using Queues/stack_using_queue.py
@@ -24,3 +24,38 @@ class Queue:
         if self.data is None:
             return True
         return False
+    
+class MyStack:
+    def __init__(self):
+        self.head = Queue()
+        self.tail = Queue()
+    def push(self, x: int) -> None:
+        while self.head.data is not None:
+            popped_head = self.head.pop()
+            self.tail.push(popped_head)
+        self.tail.push(x)
+        while self.tail.data is not None:
+            popped_tail = self.tail.pop()
+            self.head.push(popped_tail)
+
+    def pop(self) -> int:
+        return self.head.pop()
+
+    def top(self) -> int:
+        curr = self.head
+        while curr.next is not None:
+            curr = curr.next
+        return curr.data
+
+    def empty(self) -> bool:
+        if self.head.data is None:
+            return True
+        return False
+ 
+
+stack = MyStack()
+stack.push(1)
+stack.push(2)
+print(stack.top())
+print(stack.pop())
+print(stack.empty())
