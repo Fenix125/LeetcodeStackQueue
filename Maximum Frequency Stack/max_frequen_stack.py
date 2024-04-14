d58c68ae3d660f0d6640d42dfc9b66fe80ae4b19
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-14 12:29:17 +0300

    working code for basic test, remaked CustomDict, made frequency func and most_frequency_and_top to check if value can be deleted according to rules

--- Maximum Frequency Stack/max_frequen_stack.py
+++ Maximum Frequency Stack/max_frequen_stack.py
@@ -18,16 +18,32 @@ class CustomDict:
             except AttributeError:
                 previous.next = CustomDict(val)
                 previous.next.freq += 1
-                
-    def pop(self):
-        pass
-
-
+    def frequency(self, val):
+        curr = self
+        while curr.data != val:
+            curr = curr.next
+        return curr.freq
+    def most_frequency_and_top(self, curr_stack):
+        top_len = 1
+        max_freq = 0
+        max_top_len = 0
+        most_freq_value = None
+        curr = curr_stack
+        while curr is not None:
+            if self.frequency(curr.data) >= max_freq:
+                if top_len > max_top_len:
+                    most_freq_value = curr.data
+                    max_freq = self.frequency(curr.data)
+                    max_top_len = top_len
+            curr = curr.next
+            top_len += 1
+        return most_freq_value
+        
 class FreqStack:
+    dict_freq = CustomDict()
     def __init__(self, data= None):
         self.data = data
         self.next = None 
-        self.dict_freq = CustomDict()
     def push(self, val: int) -> None:
         self.dict_freq.push(val)
         if self.data is None:
@@ -43,13 +59,30 @@ class FreqStack:
             self.data = None
             return popped.data
         curr = self
-        while curr.next.next is not None:
-            curr = curr.next
-        popped = FreqStack(curr.next.data)
-        curr.next = None
-        return popped.data
+        most_frequent_val = self.dict_freq.most_frequency_and_top(self)
+        encounter = 0
+        previous = None
+        while curr:
+            if curr.data == most_frequent_val:
+                if (encounter + 1) == self.dict_freq.frequency(curr.data):
+                    previous.next = curr.next
+                    curr_dict = self.dict_freq
+                    while curr_dict.data != most_frequent_val:
+                        curr_dict = curr_dict.next
+                    curr_dict.freq -= 1
+                    return most_frequent_val
+                else:
+                    encounter += 1
+                    curr = curr.next
+            else:
+                previous = curr
+                curr = curr.next
     
-cust_dict = CustomDict()
-cust_dict.push(1)
-cust_dict.push(2)
-cust_dict.push(1)
+freqStack = FreqStack()
+freqStack.push(5)
+freqStack.push(7)
+freqStack.push(5)
+freqStack.push(7)
+freqStack.push(4)
+print(freqStack.pop())
+print('a')
