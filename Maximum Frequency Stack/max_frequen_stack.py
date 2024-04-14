faedccb4d04db0187f9ada6d38d41e2cb4f84698
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-14 10:34:48 +0300

    created a CustomDict class, to contain frequency of int being in FreqStack

--- Maximum Frequency Stack/max_frequen_stack.py
+++ Maximum Frequency Stack/max_frequen_stack.py
@@ -1,8 +1,35 @@
+class CustomDict:
+    def __init__(self, data= None) -> None:
+        self.data = data
+        self.freq = 0
+        self.next = None
+    def push(self, val: int):
+        if self.data is None:
+            self.data = val
+            self.freq += 1
+        else:
+            previous = None
+            curr = self
+            try:
+                while curr.data != val:
+                    previous = curr
+                    curr = curr.next
+                curr.freq += 1
+            except AttributeError:
+                previous.next = CustomDict(val)
+                previous.next.freq += 1
+                
+    def pop(self):
+        pass
+
+
 class FreqStack:
     def __init__(self, data= None):
         self.data = data
         self.next = None 
+        self.dict_freq = CustomDict()
     def push(self, val: int) -> None:
+        self.dict_freq.push(val)
         if self.data is None:
             self.data = val
         else:
@@ -21,3 +48,8 @@ class FreqStack:
         popped = FreqStack(curr.next.data)
         curr.next = None
         return popped.data
+    
+cust_dict = CustomDict()
+cust_dict.push(1)
+cust_dict.push(2)
+cust_dict.push(1)
